cb72e45538bbcfd608a5d60a92eace987a0f5f7a
wahidnur659 <wahidnurrohim@students.amikom.ac.id>
2025-04-08 17:41:21 +0700

    update slider

--- javascript/swiper.js
+++ javascript/swiper.js
@@ -1,32 +1,37 @@
 new Swiper('.item-slider', {
-    loop: true,
-    spaceBetween: 30,
-  
-    // If we need pagination
-    pagination: {
+  loop: true,
+  spaceBetween: 30,
+
+  autoplay: {
+    delay: 3000, 
+    disableOnInteraction: false, 
+  },
+
+
+  pagination: {
       el: '.swiper-pagination',
       clickable: true,
       dynamicBullets: true,
-    },
-  
-    // Navigation arrows
-    navigation: {
+  },
+
+  navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
-    },
-    
-    // responsive
-    breakpoints:{
-        0:{
-            slidesPreview: 1
-        },
-        720:{
-            slidesPreview: 2
-        },
-        1024:{
-            slidesPreview: 3
-        },
-    },
+  },
 
 
-  });
+  loopedSlides: 4, 
+  watchSlidesProgress: true,
+
+  breakpoints: {
+      0: {
+          slidesPerView: 1
+      },
+      720: {
+          slidesPerView: 2
+      },
+      1024: {
+          slidesPerView: 3
+      },
+  }
+});
